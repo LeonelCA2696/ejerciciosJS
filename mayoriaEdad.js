e402fc43fcdca0aeb209462e4afe9f7d830a1bfa
root <root@DESKTOP-QU8G9GV.localdomain>
2022-06-16 20:57:47 -0400

    Inicio del proyecto y primer ejercicio completado

---
+++ mayoriaEdad.js
@@ -0,0 +1,20 @@
+/**Ej1 -Pide la edad y si es mayor de 18 años indica que ya puede conducir. */
+
+function edadConduccion() {
+  const inputEdad = document.getElementById("ej1");
+  const edad = inputEdad.value;
+  let messageD;
+
+  if (edad >= 18) {
+    messageD = "Usted es mayor de edad, puede conducir.";
+  } else {
+    messageD = "Usted no es mayor de edad, no puede conducir.";
+  }
+  
+  const drivingMessage = document.getElementById("driving-authorization");
+  drivingMessage.innerText = messageD;
+}
+
+
+
+
